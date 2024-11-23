37724bf44c62643a51fbc21f1a0f3812a92586ab
Gururaj S <gururaj.sudhindra@gmail.com>
2024-11-23 19:28:14 +0530

    Update weather_report.js

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,7 +1,7 @@
 function showweatherDetails(event) {
     event.preventDefault();
     const city = document.getElementById('city').value;
-    const apiKey = '36b8a71a02f1181df55ccd38c17219a2'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = 'xxxxxx'; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
